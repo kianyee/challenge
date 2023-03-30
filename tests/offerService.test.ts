561263b9c79c4af26f2fef02b0e11585ae90c45d
Lau KY <kian_yee.lau@mercedes-benz.com>
2023-03-30 18:30:55 +0800

    add test

---
+++ tests/offerService.test.ts
@@ -0,0 +1,19 @@
+import { getDiscount } from "../src/offers/service";
+
+describe('offer time service', () => {
+    test('should calculate time correctly', () => {
+        expect(getDiscount(50, 30, '')).toBe(0)
+        expect(getDiscount(50, 30, 'OFR001')).toBe(0)
+        expect(getDiscount(50, 210, 'OFR001')).toBe(0)
+        expect(getDiscount(50, 80, 'OFR001')).toBe(10)
+        expect(getDiscount(50, 80, 'OFR00999')).toBe(0)
+
+        expect(getDiscount(40, 80, 'OFR002')).toBe(0)
+        expect(getDiscount(40, 280, 'OFR002')).toBe(0)
+        expect(getDiscount(50, 110, 'OFR002')).toBe(7)
+
+        expect(getDiscount(40, 80, 'OFR003')).toBe(0)
+        expect(getDiscount(40, 280, 'OFR003')).toBe(0)
+        expect(getDiscount(50, 110, 'OFR003')).toBe(5)
+    })
+})
