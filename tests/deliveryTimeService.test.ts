8e327268ca9b5ce352a3ce2f3f47d32e6cc8dad1
Lau KY <kian_yee.lau@mercedes-benz.com>
2023-03-30 18:08:36 +0800

    add test

--- tests/deliveryTimeService.test.ts
+++ tests/deliveryTimeService.test.ts
@@ -1,10 +1,13 @@
-import { calculateTimePerTrip, getDeliveryDetailById } from "../src/deliveryTime/service";
+import { calculateTimePerTrip, generateDeliveryCostAndTime, getDeliveryDetailById } from "../src/deliveryTime/service";
+import { baseDeliveryCose, inputChallenge2, maxSpeed } from "../src/input";
 import { DeliveryCostAndTime } from "../src/interface";
 
 const deliveryCostAndTimeList: DeliveryCostAndTime[] = []
+const testId = 'PKG1'
+const discount = 3
 deliveryCostAndTimeList.push(
     {
-        id: 'PKG1',
+        id: testId,
         discount: 3,
         totalCost: 50,
         time: 1.2
@@ -14,11 +17,16 @@ deliveryCostAndTimeList.push(
 describe('delivery time service', () => {
     test('should calculate time correctly', () => {
         expect(calculateTimePerTrip(70, 125)).toBe("1.78")
-        // expect(calculateTimePerTrip(125, 125)).toBe("1.00")
     })
 
     test('should calculate time correctly', () => {
-        expect(getDeliveryDetailById(deliveryCostAndTimeList, 'PKG1')?.discount).toBe(3)
+        expect(getDeliveryDetailById(deliveryCostAndTimeList, testId)?.discount).toBe(discount)
         expect(getDeliveryDetailById(deliveryCostAndTimeList, 'PKG999')?.discount).toBeFalsy()
+    })
+
+    test('should generate delivery const and time correctly', () => {
+        const deliveryCostAndTimeList = generateDeliveryCostAndTime(inputChallenge2, baseDeliveryCose, maxSpeed)
+        expect(deliveryCostAndTimeList?.length).toBe(inputChallenge2?.length)
+        expect(deliveryCostAndTimeList[0]?.time).toBe(calculateTimePerTrip(maxSpeed, 30))
     })
 })
